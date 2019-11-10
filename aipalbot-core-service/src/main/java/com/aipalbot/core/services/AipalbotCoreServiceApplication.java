42108b1804551c62823837960d126215e7efecf8
succesabbey <39390468+succesabbey@users.noreply.github.com>
2019-11-10 14:21:22 -0500

    my first project commit

---
+++ aipalbot-core-service/src/main/java/com/aipalbot/core/services/AipalbotCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aipalbot.core.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipalbotCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipalbotCoreServiceApplication.class, args);
+	}
+
+}
---
+++ aipalbot-core-service/src/test/java/com/aipalbot/core/services/AipalbotCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipalbot.core.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipalbotCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
